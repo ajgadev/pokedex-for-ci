1c4741234df18b88611bb2b7b79e00a982949961
Alejandro Guevara <alejandroguevara@alejandros-mbp.speedport.ip>
2024-05-05 13:04:50 +0200

    add healthcheck and rollback if not working

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
-
+const {version} = require('./package.json')
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 4000
+
+app.get('/heatlh', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
